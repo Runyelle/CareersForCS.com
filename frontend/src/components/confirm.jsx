48ccc92f3c2072988bdb28f5861c6c9d122b8d22
Runyelle <nyle15318@gmail.com>
2025-06-20 12:10:01 -0500

    finished reset password section
    *Finished reset password section
    *Will fix and change page styling later on

--- frontend/src/components/confirm.jsx
+++ frontend/src/components/confirm.jsx
@@ -2,34 +2,50 @@ import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../components/confirm.css';
+import New from '../components/newpass'
 
 export default function Confirm({ email }){
     const navigate = useNavigate();
     const [code, setCode] = useState('');
+    const [submitted, setSubmitted] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('User entered code', {code});
         /* must set up with api and backend stuff*/
 
-        if(code.length === 6){
+        if(code.length === 5){
             alert('Code accepted');
+            setSubmitted(true);
         }
         else{
-            alert('Please enter a valid 6-digit code');
+            alert('Please enter a valid 5-digit code');
         }
     };
+
+    if(submitted){
+        return(
+            <div className = "confirm-container">
+                <New />
+            </div>
+        )
+    }
+
+    
     return(
         <div className = "confirm-container">
+            <h2>CareersforCS.com</h2>
             <h2>Please check your email</h2>
-            <p>Enter the 6-digit code we sent to <strong>{email}</strong></p>
+            <p>Enter the 5-digit code we sent to <strong>{email}</strong></p>
 
             <form onSubmit = {handleSubmit}>
                 <input 
                     type ="text"
+                    inputmode="numeric"
+                    pattern="[0-9]*"
                     value={code}
-                    onChange={(e) => setCode(e.target.value)}
-                    maxLength = {6}
+                    onChange={(e) => setCode(e.target.value.replace(/\D/, ''))}
+                    maxLength = {5}
                     placeholder="Enter code"
                     className="confirm-input"
                     required
---
+++ frontend/src/components/newpass.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../components/newpass.css';
+
+export default function New(){
+    const navigate = useNavigate();
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const handlesubmit = (e) => {
+        e.preventDefault();
+
+        if(newPassword.length >= 8 && newPassword === confirmPassword){
+            alert('Password accepted');
+            navigate('/login');
+        }
+        else if(newPassword.length < 8){
+            alert('Password needs to be a minimum of 8 characters long')
+        }
+        else if(newPassword !== confirmPassword){
+            alert('New password does not match confirmation')
+        }
+    };
+    return(
+        <div className = "newpass-container">
+            <h2>CareersforCS.com</h2>
+            <h1><strong>Change Password</strong></h1>
+            <p>Password must be <strong>8-60 characters</strong></p>
+            <div>
+                <form onSubmit = {handlesubmit} className="newpass-form">
+                    <input 
+                        type = "password"
+                        inputmode = "numeric"
+                        value = {newPassword}
+                        max={60}
+                        min={8}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                        placeholder="Enter new Password"
+                        className="newpass-input"
+                        required
+                    />
+                    
+                    <input 
+                        type = "password"
+                        inputmode = "numeric"
+                        value = {confirmPassword}
+                        max={60}
+                        min={8}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        placeholder="Confirm Password"
+                        className="newpass-input"
+                        required
+                    />
+                    <button type="submit" className="newpass-button">Change Password</button>
+                </form>
+            </div>
+        </div>
+    );
+}
