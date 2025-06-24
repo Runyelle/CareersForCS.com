172b24e06c1083fe7781fc4ed73c0700200fe256
Runyelle <nyle15318@gmail.com>
2025-06-24 17:24:21 -0500

    Added stuff to sign in page
    *Added stuff to sign in page
    *Will do styling later

--- frontend/src/components/newAccount.jsx
+++ frontend/src/components/newAccount.jsx
@@ -7,7 +7,7 @@ export default function NewAccount(){
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [confirm, setConfirm] = useState('')
+    const [username, setUsername] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -16,7 +16,39 @@ export default function NewAccount(){
     };
 
     return(
-        <p> hello gyatt</p>
+        <div className = "NewAccount-page">
+            <div className = "NewAccount-left">
+                <form onSubmit = {handleSubmit} className = "NewAccount-form">
+                    <h1>Getting Started</h1>
+                    <p>Already have an account? <Link to= '/login'>Sign in</Link></p>
+                    <h2>Create a new account</h2>
+                    <label>
+                        <strong>Username</strong>
+                        <input 
+                            type="text"
+                            value={username}
+                            placeholder='John Doe'
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </label>
+
+                    <label>
+                        <strong>Password</strong>
+                        <input 
+                            type="password"
+                            value={password}
+                            placeholder='*********'
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <button type="submit">Create Account</button>
+                </form>
+            </div>
+            <div className = "NewAccount-right">
+            </div>
+        </div>
     );
 }
 
